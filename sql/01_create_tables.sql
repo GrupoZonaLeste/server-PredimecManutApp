14ce508036e342e991f2fe9316aa1a0bb0d14a81
AndradeReryson <reryson.andrade@fatec.sp.gov.br>
2025-04-29 18:26:48 -0300

    [add](ConexaoBanco): scripts sql para criar e popular o banco

---
+++ sql/01_create_tables.sql
@@ -0,0 +1,81 @@
+CREATE TABLE cliente(
+	id SERIAL PRIMARY KEY,
+	data_criacao TIMESTAMPTZ NOT NULL,
+	nome VARCHAR(100) NOT NULL
+);
+
+CREATE TYPE tipo_funcionario AS ENUM ('funcionario', 'admin');
+CREATE TABLE funcionario(
+	id SERIAL PRIMARY KEY,
+	data_criacao TIMESTAMPTZ NOT NULL,
+	nome VARCHAR(100) UNIQUE NOT NULL,
+	login VARCHAR(255) UNIQUE NOT NULL,
+	senha TEXT NOT NULL,
+	tipo tipo_funcionario NOT NULL
+);
+
+CREATE TABLE manutencao(
+	id SERIAL PRIMARY KEY,
+	data_criacao TIMESTAMPTZ NOT NULL,
+	cliente_id INT,
+	funcionario_id INT,
+	CONSTRAINT fk_manutencao_cliente
+		FOREIGN KEY(cliente_id) REFERENCES cliente(id)
+		ON DELETE CASCADE,
+	CONSTRAINT fk_manutencao_funcionario
+		FOREIGN KEY(funcionario_id) REFERENCES funcionario(id)
+		ON DELETE CASCADE
+);
+
+CREATE TABLE equipamento(
+	id SERIAL PRIMARY KEY,
+	data_criacao TIMESTAMPTZ NOT NULL,
+	nome VARCHAR(150) NOT NULL,
+	descricao TEXT,
+	manutencao_id INT,
+	CONSTRAINT fk_equip_manutencao
+		FOREIGN KEY(manutencao_id) REFERENCES manutencao(id)
+		ON DELETE CASCADE
+);
+
+CREATE TABLE foto(
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50) UNIQUE NOT NULL,
+	caminho VARCHAR(255) NOT NULL,
+	equipamento_id INT,
+	CONSTRAINT fk_foto_equipamento
+		FOREIGN KEY(equipamento_id) REFERENCES equipamento(id)
+		ON DELETE CASCADE
+);
+
+CREATE TABLE troca(
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE equipamento_troca(
+	equipamento_id INT,
+	troca_id INT,
+	CONSTRAINT fk_equipTroca_equipamento
+		FOREIGN KEY(equipamento_id) REFERENCES equipamento(id)
+		ON DELETE CASCADE,
+	CONSTRAINT fk_equipTroca_troca
+		FOREIGN KEY(troca_id) REFERENCES troca(id)
+		ON DELETE CASCADE
+);
+
+/*
+DROP TYPE tipo_funcionario CASCADE;
+DROP TABLE equipamento_troca CASCADE;
+DROP TABLE troca CASCADE;
+DROP TABLE foto CASCADE;
+DROP TABLE equipamento CASCADE;
+DROP TABLE manutencao CASCADE;
+DROP TABLE funcionario CASCADE;
+DROP TABLE cliente CASCADE;
+*/
+
+
+
+
+
